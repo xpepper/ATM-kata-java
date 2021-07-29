88c580d6f49c404f30f2c4cc9e4358cb55ca891d
Pietro Di Bello <pierodibello@gmail.com>
2021-07-29 18:38:32 +0200

    has one 5 euro banknote after loading it

--- src/main/java/kata/atm/AutomatedTellerMachine.java
+++ src/main/java/kata/atm/AutomatedTellerMachine.java
@@ -1,7 +1,15 @@
 package kata.atm;
 
+import java.util.HashMap;
+
 public class AutomatedTellerMachine {
+    private final HashMap<Integer, Integer> bills = new HashMap<>();
+
     public int billsFor(int denomination) {
-        return 0;
+        return bills.getOrDefault(denomination, 0);
+    }
+
+    public void loadBills(int denomination, int quantity) {
+        bills.put(denomination, quantity);
     }
 }
--- src/test/java/kata/atm/AutomatedTellerMachineTest.java
+++ src/test/java/kata/atm/AutomatedTellerMachineTest.java
@@ -7,8 +7,16 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class AutomatedTellerMachineTest {
 
     @Test
-    void initially_has_no_banknotes() {
+    void initially_has_no_5_euro_banknotes() {
         AutomatedTellerMachine atm = new AutomatedTellerMachine();
         assertThat(atm.billsFor(5)).isEqualTo(0);
     }
+
+    @Test
+    void has_one_5_euro_banknote_after_loading_it() {
+        AutomatedTellerMachine atm = new AutomatedTellerMachine();
+        atm.loadBills(5, 1);
+        assertThat(atm.billsFor(5)).isEqualTo(1);
+    }
+
 }
