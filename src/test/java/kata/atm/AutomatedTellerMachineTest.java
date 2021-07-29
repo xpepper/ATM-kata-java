e8a71b3ce880b1bb9f9df3b5409d56512b5d5b86
Pietro Di Bello <pierodibello@gmail.com>
2021-07-29 18:39:42 +0200

    r extract constant to avoid using a "magic number" for the 5 euro denomination

--- src/test/java/kata/atm/AutomatedTellerMachineTest.java
+++ src/test/java/kata/atm/AutomatedTellerMachineTest.java
@@ -6,17 +6,21 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class AutomatedTellerMachineTest {
 
+    public static final int FIVE_EURO = 5;
+
     @Test
     void initially_has_no_5_euro_banknotes() {
         AutomatedTellerMachine atm = new AutomatedTellerMachine();
-        assertThat(atm.billsFor(5)).isEqualTo(0);
+
+        assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(0);
     }
 
     @Test
     void has_one_5_euro_banknote_after_loading_it() {
         AutomatedTellerMachine atm = new AutomatedTellerMachine();
-        atm.loadBills(5, 1);
-        assertThat(atm.billsFor(5)).isEqualTo(1);
+        atm.loadBills(FIVE_EURO, 1);
+
+        assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(1);
     }
 
 }
