8160da2146971609ac22228c21b8413100546435
Pietro Di Bello <pierodibello@gmail.com>
2021-07-29 19:03:13 +0200

    keep track of other denomination: 10, 20, 50

--- src/test/java/kata/atm/AutomatedTellerMachineTest.java
+++ src/test/java/kata/atm/AutomatedTellerMachineTest.java
@@ -8,6 +8,10 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class AutomatedTellerMachineTest {
 
     public static final int FIVE_EURO = 5;
+    public static final int TEN_EURO = 10;
+    public static final int TWENTY_EURO = 20;
+    public static final int FIFTY_EURO = 50;
+
     private AutomatedTellerMachine atm;
 
     @BeforeEach
@@ -25,6 +29,27 @@ public class AutomatedTellerMachineTest {
         atm.loadBills(FIVE_EURO, 1);
 
         assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(1);
+    }
+
+    @Test
+    void initially_has_no_banknotes() {
+        assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(0);
+        assertThat(atm.billsFor(TEN_EURO)).isEqualTo(0);
+        assertThat(atm.billsFor(TWENTY_EURO)).isEqualTo(0);
+        assertThat(atm.billsFor(FIFTY_EURO)).isEqualTo(0);
+    }
+
+    @Test
+    void has_one_banknote_for_each_denomination_after_loading_it() {
+        atm.loadBills(FIVE_EURO, 1);
+        atm.loadBills(TEN_EURO, 1);
+        atm.loadBills(TWENTY_EURO, 1);
+        atm.loadBills(FIFTY_EURO, 1);
+
+        assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(1);
+        assertThat(atm.billsFor(TEN_EURO)).isEqualTo(1);
+        assertThat(atm.billsFor(TWENTY_EURO)).isEqualTo(1);
+        assertThat(atm.billsFor(FIFTY_EURO)).isEqualTo(1);
     }
 
 }
