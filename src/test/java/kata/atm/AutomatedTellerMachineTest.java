f5fbb8a79708a1aa7a88948014348bbfae75bd25
Pietro Di Bello <pierodibello@gmail.com>
2021-07-29 18:43:38 +0200

    r extract field

--- src/test/java/kata/atm/AutomatedTellerMachineTest.java
+++ src/test/java/kata/atm/AutomatedTellerMachineTest.java
@@ -1,5 +1,6 @@
 package kata.atm;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -7,17 +8,20 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class AutomatedTellerMachineTest {
 
     public static final int FIVE_EURO = 5;
+    private AutomatedTellerMachine atm;
+
+    @BeforeEach
+    void setUp() {
+        atm = new AutomatedTellerMachine();
+    }
 
     @Test
     void initially_has_no_5_euro_banknotes() {
-        AutomatedTellerMachine atm = new AutomatedTellerMachine();
-
         assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(0);
     }
 
     @Test
     void has_one_5_euro_banknote_after_loading_it() {
-        AutomatedTellerMachine atm = new AutomatedTellerMachine();
         atm.loadBills(FIVE_EURO, 1);
 
         assertThat(atm.billsFor(FIVE_EURO)).isEqualTo(1);
