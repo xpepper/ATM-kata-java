7e414f6155bdd82a7e9bcda72c26917bf26ef1ab
Pietro Di Bello <pietro.dibello@xpeppers.com>
2021-08-03 00:18:07 +0200

    R removed from Denomination all the stuff that we now have on the EuroDenominations

--- src/main/java/kata/atm/Denomination.java
+++ src/main/java/kata/atm/Denomination.java
@@ -1,29 +1,13 @@
 package kata.atm;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 import java.util.StringJoiner;
 
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
-
 public class Denomination {
-    public static final Denomination FIVE_EURO = new Denomination(5);
-    public static final Denomination TEN_EURO = new Denomination(10);
-    public static final Denomination TWENTY_EURO = new Denomination(20);
-    public static final Denomination FIFTY_EURO = new Denomination(50);
-
     public final int value;
 
     Denomination(int value) {
         this.value = value;
-    }
-
-    public static List<Denomination> all() {
-        return Denomination.values().stream()
-                .sorted(byHighestValueFirst())
-                .collect(toList());
     }
 
     @Override
@@ -44,13 +28,5 @@ public class Denomination {
         return new StringJoiner(", ", Denomination.class.getSimpleName() + "[", "]")
                 .add("value=" + value)
                 .toString();
-    }
-
-    private static List<Denomination> values() {
-        return asList(FIVE_EURO, TEN_EURO, TWENTY_EURO, FIFTY_EURO);
-    }
-
-    private static Comparator<Denomination> byHighestValueFirst() {
-        return (denomination, otherDenomination) -> otherDenomination.value - denomination.value;
     }
 }
